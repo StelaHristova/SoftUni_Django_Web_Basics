95e275559d1eedf32a81ab1413b09fe659dffd66
Stela Hristova <stela.m.hristova@gmail.com>
2024-12-08 15:49:08 +0200

    feat: added models to petsapp

--- petstagram/pets/models.py
+++ petstagram/pets/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Pet(models.Model):
+    name = models.CharField(
+        max_length=30,
+    )
+
+    personal_photo = models.URLField()
+
+    date_of_birth = models.DateField(
+        blank=True,
+        null=True,
+    )
+
+    slug = models.SlugField(
+        null=False,
+        blank=True,
+        unique=True,
+    )
+
+    def save(self, *args, **kwargs):
+        # super().save(*args, **kwargs)
+
+        if not self.slug:  # self.name, self.id -> Sasho Sashovski 2 -> slugify -> sasho-sashovski-2
+            self.slug = slugify(f"{self.name}-{self.id}")
+
+        super().save(*args, **kwargs)
